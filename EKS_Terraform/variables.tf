25ee02ffd656920d8d901070051a513234ae7bc6
Vamsi <info4devops@gmail.com>
2025-02-12 23:41:00 +0530

    added all files

--- EKS_Terraform/variables.tf
+++ EKS_Terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "ssh_key_name" {
   description = "The name of the SSH key pair to use for instances"
   type        = string
-  default     = "Blog-Key"
+  default     = "Blog-Key" # Replace with ur Terraform instance key-pair name
 }
